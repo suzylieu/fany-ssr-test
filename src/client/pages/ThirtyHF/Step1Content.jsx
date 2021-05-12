3abec4abfbda64d5c6f961daed400f371ac8ef1b
XXX <fany_lin@worldvision.org.tw>
2021-05-12 16:22:59 +0800

    WEB-656 step 1

--- src/client/pages/ThirtyHF/Step1Content.jsx
+++ src/client/pages/ThirtyHF/Step1Content.jsx
@@ -1,10 +1,184 @@
 import React from 'react';
+import { Controller } from "react-hook-form";
+
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormLabel from "@material-ui/core/FormLabel";
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 
-const Step1Content = () => {
+const Step1Content = ({ control, errors, watch, setValue }) => {
+  const eventCountList = [];
+  for (let i = 1; i < 21; i++) {
+    eventCountList.push(i);
+  }
+  const handleEventType = () => {
+    if (watch('event_type') === "O") {
+      setValue('event_time', '00:00');
+    }
+    if (watch('event_type') === "S") {
+      setValue('event_time', '13:00');
+      setValue('team_size', 6);
+      setValue('event_count', 0);
+    }
+  };
 
   return (
-    <Box p={2}>1111</Box>
+    <Box p={2}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={5} lg={3}>
+          <Controller
+            rules={{ required: true }}
+            control={control}
+            name="event_type"
+            render={
+              ({ field }) =>
+                <FormControl required error={errors.event_type ? true : false}>
+                  <FormLabel component="legend">組隊方式</FormLabel>
+                  <RadioGroup  {...field} onBlur={handleEventType}>
+                    <FormControlLabel
+                      value="O"
+                      control={<Radio color="primary" />}
+                      label="自行組隊體驗"
+                    />
+                    <FormControlLabel
+                      value="G"
+                      control={<Radio color="primary" />}
+                      label="組隊參加主場活動 7/17"
+                    />
+                    <FormControlLabel
+                      value="S"
+                      control={<Radio color="primary" />}
+                      label="個別報名主場活動現場編隊 7/17"
+                    />
+                  </RadioGroup>
+                </FormControl>
+            }
+          />
+        </Grid>
+        <Grid item xs={12} sm={7} lg={9}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} lg={3}>
+              <FormControl required fullWidth={true} error={errors.event_venue ? true : false}>
+                <FormLabel>地區</FormLabel>
+                <Controller
+                  name="event_venue"
+                  control={control}
+                  rules={{ required: true }}
+                  render={
+                    ({ field }) =>
+                      <TextField
+                        select
+                        size='small'
+                        variant="outlined"
+                        {...field}
+                      >
+                        <MenuItem value={"TP"}>北：台北松山文創園區文化廣場</MenuItem>
+                        <MenuItem value={"TC"}>中：台中勤美誠品綠園道</MenuItem>
+                        <MenuItem value={"KS"}>南：高雄大魯閣草衙道水驛廣場</MenuItem>
+                      </TextField>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            {watch('event_type') === "G" &&
+              <Grid item xs={12} sm={6} lg={3}>
+                <FormControl required fullWidth={true} error={errors.event_time ? true : false}>
+                  <FormLabel>時間</FormLabel>
+                  <Controller
+                    name="event_time"
+                    control={control}
+                    rules={{ required: true }}
+                    render={
+                      ({ field }) =>
+                        <TextField
+                          select
+                          size='small'
+                          variant="outlined"
+                          {...field}
+                        >
+                          <Box display="none">
+                            <MenuItem value={"00:00"} />
+                          </Box>
+                          <MenuItem value={"11:00"}>11:00</MenuItem>
+                          <MenuItem value={"12:00"}>12:00</MenuItem>
+                          <Box display="none">
+                            <MenuItem value={"13:00"}>13:00</MenuItem>
+                          </Box>
+                          <MenuItem value={"15:00"}>15:00</MenuItem>
+                          <MenuItem value={"16:00"}>16:00</MenuItem>
+                        </TextField>
+                    }
+                  />
+                </FormControl>
+              </Grid>
+            }
+            {watch('event_type') === "S" &&
+              <Grid item xs={12} sm={6} lg={3}>
+                <FormControl fullWidth={true}>
+                  <FormLabel>時間</FormLabel>
+                  <TextField size='small' variant="outlined" value="13:00" disabled />
+                </FormControl>
+              </Grid>
+            }
+            {(watch('event_type') === "O" || watch('event_type') === "G") &&
+              <>
+                <Grid item xs={12} sm={6} lg={3}>
+                  <FormControl required fullWidth={true} error={errors.team_size ? true : false}>
+                    <FormLabel>組隊人數</FormLabel>
+                    <Controller
+                      name="team_size"
+                      control={control}
+                      rules={{ required: true }}
+                      render={
+                        ({ field }) =>
+                          <TextField
+                            select
+                            size='small'
+                            variant="outlined"
+                            {...field}
+                          >
+                            <MenuItem value={2}>2</MenuItem>
+                            <MenuItem value={3}>3</MenuItem>
+                            <MenuItem value={4}>4</MenuItem>
+                            <MenuItem value={5}>5</MenuItem>
+                            <MenuItem value={6}>6</MenuItem>
+                          </TextField>
+                      }
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6} lg={3}>
+                  <FormControl required fullWidth={true} error={errors.event_count ? true : false}>
+                    <FormLabel>組數</FormLabel>
+                    <Controller
+                      name="event_count"
+                      control={control}
+                      rules={{ required: true }}
+                      render={
+                        ({ field }) =>
+                          <TextField
+                            select
+                            size='small'
+                            variant="outlined"
+                            {...field}
+                          >
+                            {eventCountList.map(el => <MenuItem key={el} value={el}>{el}</MenuItem>)}
+                          </TextField>
+                      }
+                    />
+                  </FormControl>
+                </Grid>
+              </>
+            }
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
--- src/client/pages/ThirtyHF/ThirtyHF.jsx
+++ src/client/pages/ThirtyHF/ThirtyHF.jsx
@@ -36,7 +36,7 @@ const defaultValues = {
   event_venue: '',
   event_time: '',
   team_size: '',
-  event_count: '',
+  event_count: 1,
   name: '',
   gender: '',
   email: '',
@@ -55,7 +55,9 @@ const ThirtyHF = () => {
   const dispatch = useDispatch();
   const camps = useSelector(state => state.camps);
   const step = useSelector(state => state.step)
-console.log(step)
+  console.log(step)
+
+  const { control, formState: { errors }, watch, setValue, trigger, getValues } = useForm({ mode: "onChange", defaultValues });
 
   useEffect(() => {
     dispatch(fetachGetCampList());
@@ -63,11 +65,11 @@ console.log(step)
 
 
   const handlePost = () => {
-    
+
   }
 
   const handleValid = () => {
-   
+
   };
 
   const handleBack = () => {
@@ -89,62 +91,62 @@ console.log(step)
 
   return (
     <form action="https://i-payment-test.worldvision.org.tw/API/Activity_Cart.aspx" method="post" id="form">
-        <Stepper activeStep={step} orientation="vertical">
-          {stepsLabel.map((label, index) => {
-            const labelProps = {};
-            return (
-              <Step key={label}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
-                <StepContent>
-                  {index === 0 && <Step1Content/>}
-                  {index === 1 && <Step2Content/>}
-                  {index === 2 && <Step3Content/>}
-                  <Box mb={2}>
-                    <Button
-                      disabled={step === 0}
-                      onClick={handleBack}
-                      className={classes.button}
-                    >
-                      上一步
+      <Stepper activeStep={step} orientation="vertical">
+        {stepsLabel.map((label, index) => {
+          const labelProps = {};
+          return (
+            <Step key={label}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+              <StepContent>
+                {index === 0 && <Step1Content control={control} errors={errors} watch={watch} setValue={setValue}/>}
+                {index === 1 && <Step2Content />}
+                {index === 2 && <Step3Content />}
+                <Box mb={2}>
+                  <Button
+                    disabled={step === 0}
+                    onClick={handleBack}
+                    className={classes.button}
+                  >
+                    上一步
                    </Button>
-                    {renderBtn()}
-                  </Box>
-                </StepContent>
-              </Step>
-            )
-          })}
-        </Stepper>
-        <input type="text" name="lang" id="lang" value="tw" /> <br />
-        <input type="text" name="tag" id="tag" value="11" /> <br />
-        <input type="text" name="pkey" id="pkey" value="20210507100414" /><br />
-        <input type="text" name="issp20" id="issp20" value="0" /><br />
-        <input type="text" name="url" id="url" value="https://m-test.worldvision.org.tw/shopPost.aspx" /><br />
-        <input type="text" name="name" id="name" value="fany測試" /><br />
-        <input type="text" name="mobile" id="mobile" value="0900000000" /><br />
-        <input type="text" name="birth" id="birth" value="2021/05/03" /><br />
-        <input type="text" name="tel" id="tel" value="" /><br />
-        <input type="text" name="oversea" id="oversea" value="0" /><br />
-        <input type="text" name="gender" id="gender" value="0" /><br />
-        <input type="text" name="idno" id="idno" value="F200000017" /><br />
-        <input type="text" name="mail" id="mail" value="fany_lin@worldvision.org.tw" /><br />
-        <input type="text" name="zipcode" id="zipcode" value="105" /><br />
-        <input type="text" name="city" id="city" value="台北市" /><br />
-        <input type="text" name="area" id="area" value="松山區" /><br />
-        <input type="text" name="address" id="address" value="民生東路4段133號6F" /><br />
-        <input type="text" name="payment" id="payment" value="3" /><br />
-        <input type="text" name="rPayment" id="rPayment" value="" /><br />
-        <input type="text" name="isEncrypt" id="isEncrypt" value="0" /><br />
+                  {renderBtn()}
+                </Box>
+              </StepContent>
+            </Step>
+          )
+        })}
+      </Stepper>
+      <input type="text" name="lang" id="lang" value="tw" /> <br />
+      <input type="text" name="tag" id="tag" value="11" /> <br />
+      <input type="text" name="pkey" id="pkey" value="20210507100414" /><br />
+      <input type="text" name="issp20" id="issp20" value="0" /><br />
+      <input type="text" name="url" id="url" value="https://m-test.worldvision.org.tw/shopPost.aspx" /><br />
+      <input type="text" name="name" id="name" value="fany測試" /><br />
+      <input type="text" name="mobile" id="mobile" value="0900000000" /><br />
+      <input type="text" name="birth" id="birth" value="2021/05/03" /><br />
+      <input type="text" name="tel" id="tel" value="" /><br />
+      <input type="text" name="oversea" id="oversea" value="0" /><br />
+      <input type="text" name="gender" id="gender" value="0" /><br />
+      <input type="text" name="idno" id="idno" value="F200000017" /><br />
+      <input type="text" name="mail" id="mail" value="fany_lin@worldvision.org.tw" /><br />
+      <input type="text" name="zipcode" id="zipcode" value="105" /><br />
+      <input type="text" name="city" id="city" value="台北市" /><br />
+      <input type="text" name="area" id="area" value="松山區" /><br />
+      <input type="text" name="address" id="address" value="民生東路4段133號6F" /><br />
+      <input type="text" name="payment" id="payment" value="3" /><br />
+      <input type="text" name="rPayment" id="rPayment" value="" /><br />
+      <input type="text" name="isEncrypt" id="isEncrypt" value="0" /><br />
 
-        <input type="text" name="Credit" id="Credit" value="" /><br />
-        <input type="text" name="ATMBank" id="ATMBank" value="{}" /><br />
-        <input type="text" name="act_item" id="act_item" value="[{'id':'157','remarks':'','item':[{'id':'1379','qty':'1','price':'333','deduction':'0','url':'https://m-test.worldvision.org.tw/donate_shop.aspx','parameter':'ID=157&637560004826280222&'}],'islocked':'0'}]" /><br />
-        <input type="text" name="prod_item" id="prod_item" value="[]" /><br />
-        <input type="text" name="reUrl" id="reUrl" value="http://192.168.120.17:3000/clubon-return" /><br />
-        <input type="text" name="SourceURL" id="SourceURL" value="https://m-test.worldvision.org.tw/shopPost.aspx" /><br />
-        <input type="text" name="code" id="code" value="02C77535EC30EE8B8E9916945D341B1A" /><br />
-        <input type="text" name="receipt" id="receipt" value="3" /><br />
-        <input type="text" name="receipt_name" id="receipt_name" value="fany測試" />
-      </form>
+      <input type="text" name="Credit" id="Credit" value="" /><br />
+      <input type="text" name="ATMBank" id="ATMBank" value="{}" /><br />
+      <input type="text" name="act_item" id="act_item" value="[{'id':'157','remarks':'','item':[{'id':'1379','qty':'1','price':'333','deduction':'0','url':'https://m-test.worldvision.org.tw/donate_shop.aspx','parameter':'ID=157&637560004826280222&'}],'islocked':'0'}]" /><br />
+      <input type="text" name="prod_item" id="prod_item" value="[]" /><br />
+      <input type="text" name="reUrl" id="reUrl" value="http://192.168.120.17:3000/clubon-return" /><br />
+      <input type="text" name="SourceURL" id="SourceURL" value="https://m-test.worldvision.org.tw/shopPost.aspx" /><br />
+      <input type="text" name="code" id="code" value="02C77535EC30EE8B8E9916945D341B1A" /><br />
+      <input type="text" name="receipt" id="receipt" value="3" /><br />
+      <input type="text" name="receipt_name" id="receipt_name" value="fany測試" />
+    </form>
   );
 };
 
